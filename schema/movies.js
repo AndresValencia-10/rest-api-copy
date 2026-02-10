b907ace250bfc6e97c297c83118173b9886299f3
AndresValencia <220809559+AndresValencia-10@users.noreply.github.com>
2026-02-09 23:00:04 -0500

    copia api rest

---
+++ schema/movies.js
@@ -0,0 +1,39 @@
+import z from 'zod';
+
+const movieSchema = z.object({
+	title: z.string({
+		invalid_type_error: 'movie title must be a string',
+		required_error: 'movie title is required',
+	}),
+	year: z.number().int().min(1900).max(2026),
+	director: z.string(),
+	duration: z.number().int().positive(),
+	rate: z.number().min(0).max(10).default(0),
+	poster: z.string().url({
+		message: 'poster must be a valid URL',
+	}),
+	genre: z.array(
+		z.enum([
+			'Action',
+			'Comedy',
+			'Drama',
+			'Horror',
+			'Romance',
+			'Crime',
+			'Sci-Fi',
+			'Documentary',
+		]),
+		{
+			required_error: 'genre is required',
+			invalid_type_error: 'genre must be an array of enum Genre',
+		},
+	),
+});
+
+export function validateMovie(object) {
+	return movieSchema.safeParse(object);
+}
+
+export function validatePartialMovie(object) {
+	return movieSchema.partial().safeParse(object);
+}
